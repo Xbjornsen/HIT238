d25cd8172af34c41ad95b22e1bb2d6de6f438564
XBjornsen <xaviertr7@gmail.com>
2018-09-01 16:50:43 +0930

    updates to everything

---
+++ js/map.js
@@ -0,0 +1,49 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(
+    document.getElementById('map'),
+    {
+      center: {
+        lat: -12.37,
+        lng: 130.87
+      },
+      zoom: 12
+     }
+  );
+  var marker = null;
+navigator.geolocation.getCurrentPosition(
+  function(position) {
+    marker = 
+    addMarker(position.coords.latitude, position.coords.longitude);
+    }
+  );
+  navigator.geolocation.watchPosition(
+  function(position) {
+    moveMarker(
+      marker,
+      position.coords.latitude, 
+      position.coords.longitude);
+    }
+  );
+}
+
+/**
+* @param {number} lat The latitude to add a marker at
+* @param {number} lng The longitude to add a marker at
+* @return {google.maps.Marker} The created marker
+**/
+function addMarker(lat, lng) {
+  var marker = new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
+  return marker;
+}
+
+/**
+* @param {google.maps.Marker} Marker A marker to move
+* @param {number} lat The latitude to add a marker at
+* @param {number} lng The longitude to add a marker at
+* @return {google.maps.Marker} The moved marker
+**/
+function moveMarker(marker, lat, lng) {
+  marker.setPosition({lat: lat, lng: lng});
+  return marker;
+}
